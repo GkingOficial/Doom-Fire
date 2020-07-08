e6ba6cd38e054c40122e75bc85cfa61737042286
Gking <talisoncaedoso@gmail.com>
2020-07-08 14:02:59 -0300

    Primeira versão do Doom Fire

--- doom_fire.c
+++ doom_fire.c
@@ -5,15 +5,14 @@
 #include <math.h>
 #include <time.h>
 
-#define fire_height 23
-#define fire_width 23
-#define DEBUG false
-#define INTENSITY 10
+#define fire_height 20
+#define fire_width 20
+#define DEBUG true
+#define INTENSITY 5
 
 const int fire_lenght = fire_height * fire_width;
 int fire_array[fire_height * fire_width];
 int fire_color[][3] = {{7, 7, 7}, {31, 7, 7}, {47, 15, 7}, {71, 15, 7}, {87, 23, 7}, {103, 31, 7}, {119, 31, 7}, {143, 39, 7}, {159, 47, 7}, {175, 63, 7}, {191, 71, 7}, {199, 71, 7}, {223, 79, 7}, {223, 87, 7}, {223, 87, 7}, {215, 95, 7}, {215, 95, 7}, {215, 103, 15}, {207, 111, 15}, {207, 119, 15}, {207, 127, 15}, {207, 135, 23}, {199, 135, 23}, {199, 143, 23}, {199, 151, 31}, {191, 159, 31}, {191, 159, 31}, {191, 167, 39}, {191, 167, 39}, {191, 175, 47}, {183, 175, 47}, {183, 183, 47}, {183, 183, 55}, {207, 207, 111}, {223, 223, 159}, {239, 239, 199}, {255, 255, 255}};
-
 
 void CreateStructure();
 void CalculatePropagation();
